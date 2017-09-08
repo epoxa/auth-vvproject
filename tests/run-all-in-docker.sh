bf45cc2dd4bfd2087ad71a7579a4b92761764c3c
epoxa <epoxa@vvproject.ru>
2017-09-09 02:17:57 +0700

    ADD: Travis tests

--- tests/run-all-in-docker.sh
+++ tests/run-all-in-docker.sh
@@ -2,6 +2,6 @@
 
 cd $(cd $(dirname $0) && pwd)/..
 
-docker/tests-up.sh
+docker/tests-up.sh # Tweak php modules and install dependencies
 
 docker-compose -f ./docker/docker-compose.yml exec php /var/www/html/docker/sh/run-tests.sh
