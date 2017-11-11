584b8f666572a12dd456e329e223fc17ff0fefff
epoxa <epoxa@vvproject.ru>
2017-11-11 18:18:26 +0700

    MOD: Tests

--- docker/sh/run-tests.sh
+++ docker/sh/run-tests.sh
@@ -2,5 +2,5 @@
 
 cd $(cd $(dirname $0) && pwd)/../../
 
-./vendor/bin/phpunit --coverage-clover /var/www/html/runtime/log/coverage.xml --whitelist /var/www/html
+./vendor/bin/phpunit --coverage-clover /var/www/html/runtime/log/coverage.xml --whitelist /var/www/html/config --whitelist /var/www/html/config/.current --whitelist /var/www/html/classes/YY --whitelist /var/www/html/classes/Translation
 #./vendor/bin/phpunit --filter test_infrastructure
